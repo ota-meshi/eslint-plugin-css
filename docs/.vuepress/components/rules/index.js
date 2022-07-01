5baf0102125c0cef8131058ecca12fb5fabd93fc
Yosuke Ota <otameshiyo23@gmail.com>
2022-07-01 21:46:31 +0000

    format

--- docs/.vuepress/components/rules/index.js
+++ docs/.vuepress/components/rules/index.js
@@ -1,119 +1,119 @@
 // eslint-disable-next-line eslint-comments/disable-enable-pair -- demo
 /* eslint-disable node/no-unsupported-features/es-syntax -- demo */
-import { Linter } from "eslint/lib/linter"
-import plugin from "../../../../"
+import { Linter } from "eslint/lib/linter";
+import plugin from "../../../../";
 
-const coreRules = Object.fromEntries(new Linter().getRules())
+const coreRules = Object.fromEntries(new Linter().getRules());
 
 const CATEGORY_TITLES = {
-    "Possible Errors": "Possible Errors",
-    "Best Practices": "Best Practices",
-    "Stylistic Issues": "Stylistic Issues",
-    "eslint-core-rules@problem": "ESLint core rules(Possible Errors)",
-    "eslint-core-rules@suggestion": "ESLint core rules(Suggestions)",
-    "eslint-core-rules@layout": "ESLint core rules(Layout & Formatting)",
-}
+  "Possible Errors": "Possible Errors",
+  "Best Practices": "Best Practices",
+  "Stylistic Issues": "Stylistic Issues",
+  "eslint-core-rules@problem": "ESLint core rules(Possible Errors)",
+  "eslint-core-rules@suggestion": "ESLint core rules(Suggestions)",
+  "eslint-core-rules@layout": "ESLint core rules(Layout & Formatting)",
+};
 const CATEGORY_INDEX = {
-    "Possible Errors": 1,
-    "Best Practices": 2,
-    "Stylistic Issues": 3,
-    "eslint-core-rules@problem": 20,
-    "eslint-core-rules@suggestion": 21,
-    "eslint-core-rules@layout": 22,
-}
+  "Possible Errors": 1,
+  "Best Practices": 2,
+  "Stylistic Issues": 3,
+  "eslint-core-rules@problem": 20,
+  "eslint-core-rules@suggestion": 21,
+  "eslint-core-rules@layout": 22,
+};
 const CATEGORY_CLASSES = {
-    base: "eslint-plugin-css-category",
-    "Possible Errors": "eslint-plugin-css-category",
-    "Best Practices": "eslint-plugin-css-category",
-    "Stylistic Issues": "eslint-plugin-css-category",
-    "eslint-core-rules@problem": "eslint-core-category",
-    "eslint-core-rules@suggestion": "eslint-core-category",
-    "eslint-core-rules@layout": "eslint-core-category",
-}
+  base: "eslint-plugin-css-category",
+  "Possible Errors": "eslint-plugin-css-category",
+  "Best Practices": "eslint-plugin-css-category",
+  "Stylistic Issues": "eslint-plugin-css-category",
+  "eslint-core-rules@problem": "eslint-core-category",
+  "eslint-core-rules@suggestion": "eslint-core-category",
+  "eslint-core-rules@layout": "eslint-core-category",
+};
 
-const allRules = []
+const allRules = [];
 
 for (const k of Object.keys(plugin.rules)) {
-    const rule = plugin.rules[k]
-    if (rule.meta.deprecated) {
-        continue
-    }
-    allRules.push({
-        classes: "eslint-plugin-css-rule",
-        category: rule.meta.docs.category,
-        ruleId: rule.meta.docs.ruleId,
-        url: rule.meta.docs.url,
-        init: "error",
-    })
+  const rule = plugin.rules[k];
+  if (rule.meta.deprecated) {
+    continue;
+  }
+  allRules.push({
+    classes: "eslint-plugin-css-rule",
+    category: rule.meta.docs.category,
+    ruleId: rule.meta.docs.ruleId,
+    url: rule.meta.docs.url,
+    init: "error",
+  });
 }
 for (const k of Object.keys(coreRules)) {
-    const rule = coreRules[k]
-    if (rule.meta.deprecated) {
-        continue
-    }
-    allRules.push({
-        classes: "eslint-core-rule",
-        category: `eslint-core-rules@${rule.meta.type}`,
-        ruleId: k,
-        url: rule.meta.docs.url,
-        init: plugin.configs.standard.rules[k] || "off",
-    })
+  const rule = coreRules[k];
+  if (rule.meta.deprecated) {
+    continue;
+  }
+  allRules.push({
+    classes: "eslint-core-rule",
+    category: `eslint-core-rules@${rule.meta.type}`,
+    ruleId: k,
+    url: rule.meta.docs.url,
+    init: plugin.configs.standard.rules[k] || "off",
+  });
 }
 
 allRules.sort((a, b) =>
-    a.ruleId > b.ruleId ? 1 : a.ruleId < b.ruleId ? -1 : 0,
-)
+  a.ruleId > b.ruleId ? 1 : a.ruleId < b.ruleId ? -1 : 0
+);
 
-export const categories = []
+export const categories = [];
 
 for (const rule of allRules) {
-    const title = CATEGORY_TITLES[rule.category]
-    let category = categories.find((c) => c.title === title)
-    if (!category) {
-        category = {
-            classes: CATEGORY_CLASSES[rule.category],
-            category: rule.category,
-            categoryOrder: CATEGORY_INDEX[rule.category],
-            title,
-            rules: [],
-        }
-        categories.push(category)
-    }
-    category.rules.push(rule)
+  const title = CATEGORY_TITLES[rule.category];
+  let category = categories.find((c) => c.title === title);
+  if (!category) {
+    category = {
+      classes: CATEGORY_CLASSES[rule.category],
+      category: rule.category,
+      categoryOrder: CATEGORY_INDEX[rule.category],
+      title,
+      rules: [],
+    };
+    categories.push(category);
+  }
+  category.rules.push(rule);
 }
 categories.sort((a, b) =>
-    a.categoryOrder > b.categoryOrder
-        ? 1
-        : a.categoryOrder < b.categoryOrder
-        ? -1
-        : a.title > b.title
-        ? 1
-        : a.title < b.title
-        ? -1
-        : 0,
-)
+  a.categoryOrder > b.categoryOrder
+    ? 1
+    : a.categoryOrder < b.categoryOrder
+    ? -1
+    : a.title > b.title
+    ? 1
+    : a.title < b.title
+    ? -1
+    : 0
+);
 
 export const DEFAULT_RULES_CONFIG = allRules.reduce((c, r) => {
-    c[r.ruleId] = r.init
-    return c
-}, {})
+  c[r.ruleId] = r.init;
+  return c;
+}, {});
 
-export const rules = allRules
+export const rules = allRules;
 
 export function getRule(ruleId) {
-    if (!ruleId) {
-        return null
+  if (!ruleId) {
+    return null;
+  }
+  for (const category of categories) {
+    for (const rule of category.rules) {
+      if (rule.ruleId === ruleId) {
+        return rule;
+      }
     }
-    for (const category of categories) {
-        for (const rule of category.rules) {
-            if (rule.ruleId === ruleId) {
-                return rule
-            }
-        }
-    }
-    return {
-        ruleId,
-        url: "",
-        classes: "",
-    }
+  }
+  return {
+    ruleId,
+    url: "",
+    classes: "",
+  };
 }
