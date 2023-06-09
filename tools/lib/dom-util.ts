314162c4063abfb65021fef37aaa38e5cc79f146
nojiritakeshi <62943988+nojiritakeshi@users.noreply.github.com>
2023-06-09 09:52:15 +0900

    Add type check for table elements (#93)

---
+++ tools/lib/dom-util.ts
@@ -0,0 +1,8 @@
+/**
+ * Checks if the given element is a HTMLTableElement.
+ */
+export function isHTMLTableElement(
+  table: Element | null
+): table is HTMLTableElement {
+  return table != null && table.tagName === "TABLE";
+}
